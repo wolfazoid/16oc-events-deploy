ccafbda6fc28477c861b06491589764840974396
wolfazoid <wolfahoward@gmail.com>
2026-04-03 16:48:38 -0500

    fix: read API key from window.SALTSHED_API_KEY instead of hardcoding

--- venues/salt-shed.js
+++ venues/salt-shed.js
@@ -2,7 +2,7 @@
   var VERSION = '1.3.0';
   var CDN = 'https://cdn.jsdelivr.net/npm/ticketmaster-venue-widget@' + VERSION + '/dist';
   var PAGES = 'https://wolfazoid.github.io/16oc-events-deploy';
-  var API_KEY = 'VlcOb6C2Y4W0iGius6pFX1Gh7a9GnKyg';
+  var API_KEY = window.SALTSHED_API_KEY;
 
   // Widget base CSS from CDN
   var widgetCss = document.createElement('link');
