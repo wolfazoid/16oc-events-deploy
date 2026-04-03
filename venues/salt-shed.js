3e7aca714b766af06ac53f9fc6af633519a832c5
wolfazoid <wolfahoward@gmail.com>
2026-04-03 14:31:02 -0500

    feat: bump widget to v1.2.0 (age restriction on card)

--- venues/salt-shed.js
+++ venues/salt-shed.js
@@ -1,5 +1,5 @@
 (function () {
-  var VERSION = '1.1.0';
+  var VERSION = '1.2.0';
   var CDN = 'https://cdn.jsdelivr.net/npm/ticketmaster-venue-widget@' + VERSION + '/dist';
   var PAGES = 'https://wolfazoid.github.io/16oc-events-deploy';
   var API_KEY = 'VlcOb6C2Y4W0iGius6pFX1Gh7a9GnKyg';
